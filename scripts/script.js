a09c52a4549f961cecd3e25d824159de60ea8e22
stoeteo001 <34337746+stoeteo001@users.noreply.github.com>
2020-09-18 11:22:47 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,7 @@
-// JavaScript Document
+var toggleButton = document.getElementsByClassName('toggle-button')[0]
+var navbarLinks = document.getElementsByName('li')[0]
+
+toggleButton.addEventListener('click', () => {
+ navbarLinks.classList.toggle('active')
+
+})
