4531462dcb03558a473fa85221058f9446e40d7a
stoeteo001 <34337746+stoeteo001@users.noreply.github.com>
2020-11-06 02:44:29 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
-var toggleButton = document.querySelector('.toggle-button');
-var navbarLinks = document.querySelector('li');
+var toggleButton = document.querySelector('.toggle-button');    //Maak een variabel voor de Selector class (.toggle-button) en noem deze toggleButton
+var navbarLinks = document.querySelector('header ul');          // maak een variabel voor de hele unordered list in de header genaamd NavbarLinks
 
-toggleButton.addEventListener('click', () => {
- navbarLinks.classList.toggle('active')
+toggleButton.addEventListener('click', () => {                  // add event wanneer je hem aan klikt maakt die een class aan die active heet. 
+ navbarLinks.classList.toggle('active')                         // deze class displayed de navigatie. door weer op de knop de klikken toggled hij hem weg 
 
 });
 
